2aaefe1e8faf298c753413b484ffe08e18ec98bd
saumya gupta <saumyagupta@gmail.com>
2022-10-16 22:11:44 +0530

    reove

--- HomeDecor-FrontEnd/src/util/AppConstants.js
+++
@@ -1,3 +0,0 @@
-//Api ends ponint
-export const baseUrl = "http://localhost:9091";
-export const ApiUrl = "http://localhost:3000";
--- src/main/java/com/OnlinehomedecorStore/Repositories/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.OnlinehomedecorStore.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.OnlinehomedecorStore.entities.CategoryModel;
-
-public interface CategoryRepository extends JpaRepository<CategoryModel, Integer> {
-}
